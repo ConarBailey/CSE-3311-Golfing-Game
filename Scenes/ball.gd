6581a457beda3aaf0a583aa47503242ede06e33c
Nna² <144557253+NEjim2001@users.noreply.github.com>
2025-10-16 07:53:10 -0500

    feat: added Golf Ball lines and upgraded Pause button

--- Scenes/ball.gd
+++ Scenes/ball.gd
@@ -5,13 +5,80 @@ signal time
 signal cameraChange(x,y)
 signal updateText(stri)
 @export var speed = 1000
-@export var shotEnable = 25
+@export var shotEnable = 5
 @export var maxforce = 70000
 var strokes = 0
 var startingx = 0
 var startingy = 0
 var TimerEnable = true
 var StrokesTotal = [0,0,0,0,0,0,0,0,0]
+
+var spawn_position
+var is_aiming = false
+var aim_start_pos = Vector2.ZERO
+var aim_current_pos = Vector2.ZERO
+var aim_line
+
+func _ready():
+	spawn_position = global_position  
+	aim_line = $AimLine
+	aim_line.clear_points()
+
+func _physics_process(delta: float) -> void:
+	if linear_velocity.length() < 10:
+		linear_velocity = Vector2.ZERO
+		angular_velocity = 0
+		
+func _process(_delta):
+	
+	if is_aiming:
+		aim_current_pos = get_global_mouse_position()
+		var direction = aim_start_pos - aim_current_pos
+		var max_length = 200  
+		var line_vector = direction.limit_length(max_length)
+		
+		aim_line.clear_points()
+		aim_line.add_point(Vector2.ZERO)
+		aim_line.add_point(to_local(aim_start_pos + line_vector))
+
+func _input(event: InputEvent) -> void:
+	# Restart level
+	if event is InputEventKey and event.pressed and event.keycode == KEY_R:
+		_restart_level()
+		return
+
+	# Start aiming
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT :
+		if event.pressed:
+			if linear_velocity.length() < shotEnable and TimerEnable:
+				is_aiming = true
+				aim_start_pos = global_position
+
+		# Release - shoot
+		else:
+			if is_aiming and linear_velocity.length() < shotEnable and TimerEnable:
+				startingx = global_position.x
+				startingy = global_position.y
+				var force = (position - get_global_mouse_position())*speed
+				if force.x > maxforce:
+					force.x = maxforce
+				if force.y > maxforce:
+					force.y = maxforce
+				if force.x < -maxforce:
+					force.x = -maxforce
+				if force.y < -maxforce:
+					force.y = -maxforce
+				apply_central_force(force)
+				##print(global_position - get_global_mouse_position())
+				strokes = strokes + 1
+				strokesChange.emit(strokes)
+				TimerEnable = false
+				time.emit()
+
+			is_aiming = false
+			aim_line.clear_points()  # Hide line
+
+
 
 func _moveBallTo(x: int,y: int) -> void:
 	startingx = x
@@ -22,26 +89,6 @@ func _moveBallTo(x: int,y: int) -> void:
 	linear_velocity.x = 0
 	linear_velocity.y = 0
 	angular_velocity = 0
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and not event.pressed and (linear_velocity.x < shotEnable and linear_velocity.y < shotEnable) and TimerEnable:
-		startingx = global_position.x
-		startingy = global_position.y
-		var force = (position - get_global_mouse_position())*speed
-		if force.x > maxforce:
-			force.x = maxforce
-		if force.y > maxforce:
-			force.y = maxforce
-		if force.x < -maxforce:
-			force.x = -maxforce
-		if force.y < -maxforce:
-			force.y = -maxforce
-		apply_central_force(force)
-		##print(global_position - get_global_mouse_position())
-		strokes = strokes + 1
-		strokesChange.emit(strokes)
-		TimerEnable = false
-		time.emit()
 
 func _on_right_body_exited(_body: Node2D) -> void:
 	linear_velocity.x = 0
@@ -118,3 +165,13 @@ func _on_hole_9_body_entered(body: Node2D) -> void:
 		for i in StrokesTotal:
 			sum += i
 		updateText.emit("Level Pack\n Complete!\nTotal Strokes\n" + str(sum))
+
+func _restart_level():
+	# Work In Progress
+	global_position = spawn_position 
+	startingx = spawn_position.x
+	startingy = spawn_position.y
+	linear_velocity = Vector2.ZERO
+	angular_velocity = 0
+	strokes = 0
+	strokesChange.emit(strokes)
